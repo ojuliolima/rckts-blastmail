ba8932841148b41820119fe9662f424f8a55df88
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-01-03 11:09:27 -0300

    feat: cria componente de card e atribui ao index da lista de email

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -5,21 +5,15 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @forelse ($emailLists as $list)
-                        
-                    @empty
-                    <div class="flex justify-center">
-                        <x-link-button :href="route('email-list.create')">
-                            {{ __('Create your first email list') }}
-                        </x-link-button>
-                    </div>
-                    @endforelse
-                </div>
-            </div>
+    <x-card>
+        @forelse ($emailLists as $list)
+                    
+        @empty
+        <div class="flex justify-center">
+            <x-link-button :href="route('email-list.create')">
+                {{ __('Create your first email list') }}
+            </x-link-button>
         </div>
-    </div>
+        @endforelse
+    </x-card>
 </x-layouts.app>
