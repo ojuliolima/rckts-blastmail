9148c05b175629f676d7702c3b36ce7921d409f8
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-02-05 12:00:12 -0300

    style: adiciona a configuração de css para o atributo secondary no componente de botão do tipo link

--- resources/views/components/button/link.blade.php
+++ resources/views/components/button/link.blade.php
@@ -1,3 +1,9 @@
-<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+@props(['secondary' => null])
+
+<a {{ $attributes->class([
+    'inline-flex items-center px-4 py-2 border rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150',
+    'bg-gray-800 dark:bg-gray-200 border-transparent  text-white dark:text-gray-800  hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300  ' => !$secondary,
+    'bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-500  text-gray-700 dark:text-gray-300 shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 disabled:opacity-25' => $secondary
+    ]) }}>
     {{ $slot }}
 </a>
