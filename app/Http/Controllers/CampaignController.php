7e319caa779ccd5078d40a363393565a81c5aa01
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-02-24 07:51:29 -0300

    feat: adiciona a lógica de validação ao for request CampaignStoreRequest

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CampaignStoreRequest;
 use App\Models\Campaign;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
@@ -13,22 +14,22 @@ class CampaignController extends Controller
         $search = request()->get('search', null);
         $withTrashed = request()->get('withTrashed', false);
 
-        return view ('campaigns.index', [
+        return view('campaigns.index', [
             'campaigns' => Campaign::query()
-            ->when($withTrashed, fn(Builder $query) => $query->withTrashed())
-            ->when($search, fn(Builder $query) => $query->where('name', 'like', "%$search%")->orWhere('id', '=', $search))
-            ->paginate(5)
-            ->appends(compact('search', 'withTrashed')),
+                ->when($withTrashed, fn(Builder $query) => $query->withTrashed())
+                ->when($search, fn(Builder $query) => $query->where('name', 'like', "%$search%")->orWhere('id', '=', $search))
+                ->paginate(5)
+                ->appends(compact('search', 'withTrashed')),
             'search' => $search,
             'withTrashed' => $withTrashed,
         ]);
     }
 
     public function create(?string $tab = null)
-    { 
+    {
         return view('campaigns.create', [
             'tab' => $tab,
-            'form' => match($tab){
+            'form' => match ($tab) {
                 'template' => '_template',
                 'schedule' => '_schedule',
                 default => '_config'
@@ -42,58 +43,21 @@ class CampaignController extends Controller
                 'body' => null,
                 'track_click' => null,
                 'track_open' => null,
-                'sent_at' => null,
+                'send_at' => null,
             ]),
         ]);
 
-        
+
     }
 
-    public function store(?string $tab = null)
-    {   
-        $toRoute = '';
-
-        $map = array_merge([
-            'name' => null,
-            'subject' => null,
-            'email_list_id' => null,
-            'template_id' => null,
-            'body' => null,
-            'track_click' => null,
-            'track_open' => null,
-            'sent_at' => null,
-        ], request()->all());
-                
-        if(blank($tab)) {
-            request()->validate([
-                'name' => ['required', 'max:255'],
-                'subject' => ['required', 'max:40'],
-                'email_list_id' => ['nullable'],
-                'template_id' => ['nullable'],
-            ]);
-            $toRoute = route('campaigns.create', ['tab' => 'template']);;
-        }
-
-        if($tab == 'template') {
-            request()->validate(['body' => ['required']]);
-            $toRoute = route('campaigns.create', ['tab' => 'schedule']);
-        }
+    public function store(CampaignStoreRequest $request, ?string $tab = null)
+    {
+        $data = $request->getData();
+        $toRoute = $request->getToRoute();
 
         if($tab == 'schedule') {
-            request()->validate(['sent_at' => ['required', 'date']]);
-            $toRoute = route('campaigns.index');
+            Campaign::create($data);
         }
-
-        $session = session('campaigns::create', $map);
-
-        foreach ($session as $key => $value) {
-            $newValue = data_get($map, $key);
-            if (filled($newValue)) {
-                $session[$key] = $newValue;
-            }
-        }
-        
-        session()->put('campaigns::create', $session);
 
         return response()->redirectTo($toRoute);
     }
