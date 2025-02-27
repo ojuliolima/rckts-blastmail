2d3b3ef0de1effa36d4b69390871840195ab54ca
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-02-27 09:24:35 -0300

    feat: cria campos de template e email list na criação de campanha

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CampaignStoreRequest;
 use App\Models\Campaign;
+use App\Models\EmailList;
+use App\Models\Template;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Traits\Conditionable;
 
 class CampaignController extends Controller
 {
+    use Conditionable;
     public function index()
     {
         $search = request()->get('search', null);
@@ -27,27 +31,31 @@ class CampaignController extends Controller
 
     public function create(?string $tab = null)
     {
-        return view('campaigns.create', [
-            'tab' => $tab,
-            'form' => match ($tab) {
-                'template' => '_template',
-                'schedule' => '_schedule',
-                default => '_config'
-            },
-
-            'data' => session()->get('campaigns::create', [
-                'name' => null,
-                'subject' => null,
-                'email_list_id' => null,
-                'template_id' => null,
-                'body' => null,
-                'track_click' => null,
-                'track_open' => null,
-                'send_at' => null,
-            ]),
-        ]);
-
-
+        return view(
+            'campaigns.create', 
+            array_merge(
+            $this->when(blank($tab), fn () => [
+                    'emailLists' => EmailList::query()->select(['id', 'title'])->orderBy('title')->get(),
+                    'templates' => Template::query()->select(['id', 'name'])->orderBy('name')->get(),
+            ], fn () => []), 
+            [
+                'tab' => $tab,
+                'form' => match ($tab) {
+                    'template' => '_template',
+                    'schedule' => '_schedule',
+                    default => '_config'
+                },
+                'data' => session()->get('campaigns::create', [
+                    'name' => null,
+                    'subject' => null,
+                    'email_list_id' => null,
+                    'template_id' => null,
+                    'body' => null,
+                    'track_click' => null,
+                    'track_open' => null,
+                    'send_at' => null,
+                ]),
+            ]));
     }
 
     public function store(CampaignStoreRequest $request, ?string $tab = null)
--- resources/views/campaigns/create/_config.blade.php
+++ resources/views/campaigns/create/_config.blade.php
@@ -11,12 +11,22 @@
     </div>
     <div>
         <x-input-label for="email_list_id" :value="__('Email List')" />
-        <x-input.text id="email_list_id" class="block mt-1 w-full" name="email_list_id" :value="old('email_list_id', $data['email_list_id'])" autofocus />
+        <x-select name="email_list_id" id="email_list_id">
+            <option value="" @if(blank(old('email_list_id', $data['email_list_id']))) selected @endif></option>
+            @foreach ($emailLists as $list)
+            <option value="{{ $list->id }}" @if(old('email_list_id', $data['email_list_id']) == $list->id) selected @endif>{{ $list->title }}</option>
+            @endforeach
+        </x-select>
         <x-input-error :messages="$errors->get('email_list_id')" class="mt-2" />
     </div>
     <div>
         <x-input-label for="template_id" :value="__('Template')" />
-        <x-input.text id="template_id" class="block mt-1 w-full" name="template_id" :value="old('template_id', $data['template_id'])" autofocus />
+        <x-select name="template_id" id="template_id">
+            <option value="" @if(blank(old('template_id', $data['template_id']))) selected @endif></option>
+            @foreach ($templates as $template)
+                <option value="{{ $template->id }}" @if(old('template_id', $data['template_id']) == $template->id) selected @endif>{{ $template->name }}</option>
+            @endforeach
+        </x-select>
         <x-input-error :messages="$errors->get('template_id')" class="mt-2" />
     </div>
     <div>
