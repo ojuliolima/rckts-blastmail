1034535d7642e84de8884f450223c0f54711fe61
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-02-27 07:51:43 -0300

    feat: aplica middleware para controlar as abas de criação de campanha de email

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\EmailListController;
 use App\Http\Controllers\SubscriberController;
+use App\Http\Middleware\CampaignCreateSessionControl;
 
 Route::view('/', 'welcome');
 
@@ -25,7 +26,10 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('templates', TemplateController::class);
     Route::resource('campaigns', CampaignController::class)->only(['index', 'destroy']);
-    Route::get('/campaigns/create/{tab?}', [CampaignController::class, 'create'])->name('campaigns.create');
+    Route::get('/campaigns/create/{tab?}', [CampaignController::class, 'create'])
+        ->middleware(CampaignCreateSessionControl::class)
+        ->name('campaigns.create');
+        
     Route::post('/campaigns/create/{tab?}', [CampaignController::class, 'store']);
     Route::patch('/campaigns/{campaign}/restore', [CampaignController::class, 'restore'])->withTrashed()->name('campaigns.restore');
   
