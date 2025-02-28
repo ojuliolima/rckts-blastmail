ee1f9f8f1b3173b78102f32d530e0726470cc1f7
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-02-28 12:36:12 -0300

    feat: adiciona informações de envio e a opção de agenda de envio na área de Schedule

--- resources/views/campaigns/create/_schedule.blade.php
+++ resources/views/campaigns/create/_schedule.blade.php
@@ -1,6 +1,20 @@
-<div class="grid grid-cols-2 gap-4">
+<div class="flex flex-col gap-4">
     <div>
-        <x-input-label for="send_at" :value="__('Sent at')" />
+        <div>{{ __('From') }}:----@---.com</div>
+        <div>{{ __('To') }}: #count de emails do email_list_id</div>
+        <div>{{ __('Subject:') }} {{ $data['subject'] }}</div>
+        <div>Template: #template</div>
+    </div>
+    <hr/>
+        <div class="flex flex-col gap-2">
+            <x-input-label :value="__('Schedule Delivery')" class="mt-2" />
+            <x-input.radio id="send_now" name="send_when">{{ __('Send Now') }}</x-input.radio>
+            <x-input.radio id="send_later" name="send_when">{{ __('Send Later') }}</x-input.radio>
+        </div>
+    </div>
+
+    <div>
+        <x-input-label for="send_at" :value="__('Send at')" />
         <x-input.text id="send_at" class="block mt-1 w-full" type="date" name="send_at" :value="old('send_at', $data['send_at'])" autofocus />
         <x-input-error :messages="$errors->get('send_at')" class="mt-2" />
     </div>
