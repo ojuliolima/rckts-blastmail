0b8b511b458c80fe9b24231d3d104cb4dfd45018
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-02-28 08:44:09 -0300

    refactor: transforma os componentes de track open e track click em componentes do tipo checkbox

--- app/Http/Requests/CampaignStoreRequest.php
+++ app/Http/Requests/CampaignStoreRequest.php
@@ -39,10 +39,9 @@ class CampaignStoreRequest extends FormRequest
             $rules = [
                 'name' => ['required', 'max:255'],
                 'subject' => ['required', 'max:40'],
-                'email_list_id' => ['nullable'],
-                'template_id' => ['nullable'],
+                'email_list_id' => ['required', 'exists:email_lists,id'],
+                'template_id' => ['required', 'exists:templates,id'],
             ];
-
         }
 
         if ($tab == 'template') {
@@ -56,8 +55,11 @@ class CampaignStoreRequest extends FormRequest
         $session = session('campaigns::create', $map);
 
         foreach ($session as $key => $value) {
+
             $newValue = data_get($map, $key);
-            if (filled($newValue)) {
+            if($key == 'track_click' || $key == 'track_open') {
+                $session[$key] = $newValue;
+            }elseif (filled($newValue)) {
                 $session[$key] = $newValue;
             }
         }
@@ -71,6 +73,8 @@ class CampaignStoreRequest extends FormRequest
     {
         $session = session()->get('campaigns::create');
         unset($session['_token']);
+        $session['track_click'] = $session['track_click'] ?: false;
+        $session['track_open'] = $session['track_open'] ?: false;
 
         return $session;
     }
--- resources/views/campaigns/create/_config.blade.php
+++ resources/views/campaigns/create/_config.blade.php
@@ -30,13 +30,11 @@
         <x-input-error :messages="$errors->get('template_id')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="track_click" :value="__('Track Click')" />
-        <x-input.text id="track_click" class="block mt-1 w-full" name="track_click" :value="old('track_click', $data['track_click'])" autofocus />
+        <x-input.checkbox id="track_click" name="track_click" autofocus value="1" :isCheckedWhen="old('track_click', $data['track_click'])" :label="__('Track Click')"/>  
         <x-input-error :messages="$errors->get('track_click')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="track_open" :value="__('Track Open')" />
-        <x-input.text id="track_open" class="block mt-1 w-full" name="track_open" :value="old('track_open', $data['track_open'])" autofocus />
+        <x-input.checkbox id="track_open" name="track_open" autofocus value="1" :isCheckedWhen="old('track_open', $data['track_open'])" :label="__('Track Open')"/>
         <x-input-error :messages="$errors->get('track_open')" class="mt-2" />
     </div>
 </div>
