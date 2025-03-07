c6b95f78941d7a6be68fd33ad2a0e39ecd4e6a9a
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-07 10:15:01 -0300

    feat: adiciona campo de seleção de data de envio na aba de Schedule

--- resources/views/campaigns/create/_schedule.blade.php
+++ resources/views/campaigns/create/_schedule.blade.php
@@ -6,16 +6,15 @@
         <div>Template: #template</div>
     </div>
     <hr/>
-        <div class="flex flex-col gap-2">
-            <x-input-label :value="__('Schedule Delivery')" class="mt-2" />
-            <x-input.radio id="send_now" name="send_when">{{ __('Send Now') }}</x-input.radio>
-            <x-input.radio id="send_later" name="send_when">{{ __('Send Later') }}</x-input.radio>
+    <div x-data="{ show: '{{ data_get($data, 'send_when', 'now') }}'}">
+        <x-input-label :value="__('Schedule Delivery')" />
+        <div class="flex flex-col gap-2 my-2">
+            <x-input.radio x-model="show" value="now" id="send_now" name="send_when">{{ __('Send Now') }}</x-input.radio>
+            <x-input.radio x-model="show" value="later" id="send_later" name="send_when">{{ __('Send Later') }}</x-input.radio>
         </div>
-    </div>
-
-    <div>
-        <x-input-label for="send_at" :value="__('Send at')" />
-        <x-input.text id="send_at" class="block mt-1 w-full" type="date" name="send_at" :value="old('send_at', $data['send_at'])" autofocus />
-        <x-input-error :messages="$errors->get('send_at')" class="mt-2" />
+        <div x-show="show == 'later'">
+            <x-input.text id="send_at" class="block mt-1 w-full" type="date" name="send_at" :value="old('send_at', $data['send_at'])" autofocus />
+            <x-input-error :messages="$errors->get('send_at')" class="mt-2" />
+        </div>
     </div>
 </div>
