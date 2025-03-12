32924405a9963c9128eea22b584bc41fe2a24788
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-12 10:57:29 -0300

    feat: cria o layout da página de estatísticas

--- resources/views/campaigns/show/_statistics.blade.php
+++ resources/views/campaigns/show/_statistics.blade.php
@@ -1,3 +1,11 @@
-<x-card>
-    statistics
-</x-card>
+<div class="gap-4 flex-col flex">
+    <x-alert success no-icon title='Your campaign is ready to be send!'/>
+    <div class="grid grid-cols-3 gap-5">
+        <x-dashboard.card heading="01" subheading="{{ __('Opens') }}"/>
+        <x-dashboard.card heading="02" subheading="{{ __('Unique Opens') }}"/>
+        <x-dashboard.card heading="20%" subheading="{{ __('Open rate') }}"/>
+        <x-dashboard.card heading="0" subheading="{{ __('Clicks') }}"/>
+        <x-dashboard.card heading="0" subheading="{{ __('Unique Clicks') }}"/>
+        <x-dashboard.card heading="20%" subheading="{{ __('Clicks Rate') }}"/>
+    </div>
+</div>
