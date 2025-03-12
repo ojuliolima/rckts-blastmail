eb52ce56ad9b818c37dcf98bbb9b7e54dfd68fdd
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-12 10:56:50 -0300

    refactor: adiciona a opção de ocultar o ícone do componente de alerta

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -3,7 +3,8 @@
     'success' => null,
     'info' => null,
     'warning' => null,
-    'danger' => null
+    'danger' => null,
+    'noIcon' => false
 ])
 
 <div @class([
@@ -20,6 +21,7 @@
         'bg-amber-500/10' => $warning,
         'bg-red-500/10' => $danger,
     ])>
+        @unless($noIcon)
         <div @class([
             'rounded-full p-1',
             'bg-green-500/15 text-green-500' => $success,
@@ -31,6 +33,7 @@
                 <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
             </svg>
         </div>
+        @endunless
         <div class="ml-2">
             <h3 @class([
                 'text-sm font-semibold',
