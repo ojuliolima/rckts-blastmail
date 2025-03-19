61db148cbf7db4cfbaf84d05b1cda64189e3b7f9
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-19 09:19:00 -0300

    feat: implementa a busca dos campos na página de estátisticas

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -73,7 +73,17 @@ class CampaignController extends Controller
         }
         $search = request()->search;
 
-        return view('campaigns.show', compact('campaign', 'what'));
+        $query = $campaign->mails()->selectRaw('
+            count(subscriber_id) as total_subscribers,
+            sum(openings) as total_openings,
+            count(case when openings > 0 then subscriber_id end) as unique_openings,
+            round((cast(count(case when openings > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as openings_rate,
+            sum(clicks) as total_clicks,
+            count(case when clicks > 0 then subscriber_id end) as unique_clicks,
+            round((cast(count(case when clicks > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as clicks_rate
+        ')->first();
+
+        return view('campaigns.show', compact('campaign', 'what', 'search', 'query'));
     }
 
     public function store(CampaignStoreRequest $request, ?string $tab = null)
--- resources/views/campaigns/show/_statistics.blade.php
+++ resources/views/campaigns/show/_statistics.blade.php
@@ -1,11 +1,11 @@
 <div class="gap-4 flex-col flex">
-    <x-alert success no-icon title='Your campaign is ready to be send!'/>
-    <div class="grid grid-cols-3 gap-5">
-        <x-dashboard.card heading="01" subheading="{{ __('Opens') }}"/>
-        <x-dashboard.card heading="02" subheading="{{ __('Unique Opens') }}"/>
-        <x-dashboard.card heading="20%" subheading="{{ __('Open rate') }}"/>
-        <x-dashboard.card heading="0" subheading="{{ __('Clicks') }}"/>
-        <x-dashboard.card heading="0" subheading="{{ __('Unique Clicks') }}"/>
-        <x-dashboard.card heading="20%" subheading="{{ __('Clicks Rate') }}"/>
-    </div>
+    <x-alert success no-icon :title="__('Your campaign has been sent to '. $query['total_subscribers'] .' subscribers from list '. $campaign->EmailList->title)"/>
+   <div class="grid grid-cols-3 gap-5">
+        <x-dashboard.card :heading="$query['total_openings']" subheading="{{ __('Opens') }}"/>
+        <x-dashboard.card :heading="$query['unique_openings']" subheading="{{ __('Unique Opens') }}"/>
+        <x-dashboard.card heading="{{ $query['openings_rate'] }}%" subheading="{{ __('Open rate') }}"/>
+        <x-dashboard.card :heading="$query['total_clicks']" subheading="{{ __('Clicks') }}"/>
+        <x-dashboard.card :heading="$query['unique_clicks']" subheading="{{ __('Unique Clicks') }}"/>
+        <x-dashboard.card heading="{{ $query['clicks_rate'] }}%" subheading="{{ __('Clicks Rate') }}"/>
+    </div> 
 </div>
