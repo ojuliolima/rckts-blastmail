c8f63e22a73552fe15acab3e68c414d8768e3889
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-20 07:27:13 -0300

    refactor: adiciona a consulta de busca por estatísticas no model de email de campanha, utilizando do Local Query Scope

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -73,15 +73,7 @@ class CampaignController extends Controller
         }
         $search = request()->search;
 
-        $query = $campaign->mails()->selectRaw('
-            count(subscriber_id) as total_subscribers,
-            sum(openings) as total_openings,
-            count(case when openings > 0 then subscriber_id end) as unique_openings,
-            round((cast(count(case when openings > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as openings_rate,
-            sum(clicks) as total_clicks,
-            count(case when clicks > 0 then subscriber_id end) as unique_clicks,
-            round((cast(count(case when clicks > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as clicks_rate
-        ')->first();
+        $query = $campaign->mails()->statistics();
 
         return view('campaigns.show', compact('campaign', 'what', 'search', 'query'));
     }
--- app/Models/CampaignMail.php
+++ app/Models/CampaignMail.php
@@ -2,13 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CampaignMail extends Model
 {
     /** @use HasFactory<\Database\Factories\CampaignMailFactory> */
     use HasFactory;
+
+    public function scopeStatistics(Builder $query)
+    {
+        return $query->selectRaw('
+            count(subscriber_id) as total_subscribers,
+            sum(openings) as total_openings,
+            count(case when openings > 0 then subscriber_id end) as unique_openings,
+            round((cast(count(case when openings > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as openings_rate,
+            sum(clicks) as total_clicks,
+            count(case when clicks > 0 then subscriber_id end) as unique_clicks,
+            round((cast(count(case when clicks > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as clicks_rate
+        ')->first();
+    }
 
     public function campaign()
     {
