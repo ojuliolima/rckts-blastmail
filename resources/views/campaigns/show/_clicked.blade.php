a969083ad617e3c164bc48a29ea8510bd7447828
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-24 14:57:11 -0300

    fix: corrige campo de pesquisa que perde o valor ao realizar uma requisição

--- resources/views/campaigns/show/_clicked.blade.php
+++ resources/views/campaigns/show/_clicked.blade.php
@@ -1,6 +1,6 @@
 <div class="space-y-4">
     <x-form action="{{ route('campaigns.show', ['campaign' => $campaign, 'what' => $what]) }}" method="get">
-        <x-input.text name="search" placeholder="{{ __('Search an email...') }}" value="{{ old($search) }}" />
+        <x-input.text name="search" placeholder="{{ __('Search an email...') }}" value="{{ $search }}" />
     </x-form>
     <x-table :headers="[__('Name'), __('Clicks'), __('Email')]">
         <x-slot name="body">
