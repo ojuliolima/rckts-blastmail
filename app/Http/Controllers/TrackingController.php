7f23d25f6379e2238f73e2817391735daae9bd93
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-03-27 16:18:55 -0300

    feat: adiciona a quantidade de cliques e abertura apenas para as campanhas que possuirem as respectivas tags selecionadas

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -9,13 +9,18 @@ class TrackingController extends Controller
 {
     public function openings(CampaignMail $mail)
     {
-        $mail->openings++;
-        $mail->save();
+        if($mail->campaign->track_open) {
+            $mail->openings++;
+            $mail->save();
+        }
+
     }
     public function clicks(CampaignMail $mail)
     {
-        $mail->clicks++;
-        $mail->save();
+        if($mail->campaign->track_click) {
+            $mail->clicks++;
+            $mail->save();
+        }
 
         return redirect()->away(request()->get('f'));
     }
