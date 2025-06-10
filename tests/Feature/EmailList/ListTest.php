4bedf099932955f0a0a8939890544e436f4a72a3
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-06-10 08:33:34 -0300

    feat: cria o teste para a lista de email

---
+++ tests/Feature/EmailList/ListTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\EmailList;
+
+use App\Models\EmailList;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class ListTest extends TestCase
+{
+    public function test_needs_to_be_authenticated()
+    {
+        $this->getJson(route('email-list.index'))->assertUnauthorized();
+
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->get(route('email-list.index'))->assertSuccessful();
+    }
+    public function test_it_should_be_paginate()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        EmailList::factory()->count(40)->create();
+
+        //act
+        $response = $this->get(route('email-list.index'));
+
+        //asset
+        $response->assertViewHas('emailLists', function ($list) {
+            $this->assertInstanceOf(LengthAwarePaginator::class, $list);
+            $this->assertCount(5, $list);
+
+            return true;
+        });
+    }
+
+    public function test_it_should_be_able_to_search_a_list()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        EmailList::factory()->count(10)->create();
+        EmailList::factory()->create(['title' => 'Title 1']);
+        $emailList = EmailList::factory()->create(['title' => 'Title Testing 2']);
+
+        //act
+        $response = $this->get(route('email-list.index', ['search' => 'Testing 2']));
+
+        //asset
+        $response->assertViewHas('emailLists', function ($list) use($emailList) {
+            $this->assertInstanceOf(LengthAwarePaginator::class, $list);
+            $this->assertCount(1, $list);
+            $this->assertEquals($emailList->id, $list->first()->id);
+
+            return true;
+        });
+    }
+}
