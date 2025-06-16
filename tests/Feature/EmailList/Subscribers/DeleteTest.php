1a6671cdc808e0cb7711f1d89483d5a594b5cd6f
Julio Cesar Alves de Lima <jclima@notredamecampinas.net.br>
2025-06-16 08:26:10 -0300

    feat: create test for subscriber deletion

---
+++ tests/Feature/EmailList/Subscribers/DeleteTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Subscriber;
+use function Pest\Laravel\assertSoftDeleted;
+use function Pest\Laravel\Delete;
+
+it('should be abble to delete a subscriber from a list', function() {
+    login();
+    $subscriber = Subscriber::factory()->create();
+
+    delete(
+        route('subscribers.destroy', ['emailList' => $subscriber->emailList, 'subscriber' => $subscriber])
+    )->assertRedirect();
+
+    assertSoftDeleted('subscribers', [
+        'id' => $subscriber->id
+    ]);
+});
